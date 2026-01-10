a709073ff146555e9b5ec4b70ecff36fb1ee1ae9
bassamdrivingschool <hachemmahmoud5@gmail.com>
2026-01-10 18:02:13 +0200

    update all

--- red_app.js
+++ red_app.js
@@ -196,8 +196,8 @@ function showResults() {
   document.getElementById("quizView").classList.add("hidden");
   document.getElementById("resultsView").classList.remove("hidden");
 
-  const phone = localStorage.getItem("quiz_phone") || "03836482 - 03720630";
-  document.getElementById("resultUser").textContent = `مدرسة بسام هاشم — ${phone}`;
+  const phone = localStorage.getItem("quiz_phone") || "71655506";
+  document.getElementById("resultUser").textContent = `مدرسة منير ايلي داغر— ${phone}`;
 
   const passed = quiz.score >= 24;
 
@@ -276,9 +276,9 @@ function startNewExam() {
 }
 
 async function init() {
-  const phone = localStorage.getItem("quiz_phone") || "03836482 - 03720630";
+  const phone = localStorage.getItem("quiz_phone") || "71655506";
 
-  document.getElementById("userName").textContent = "مدرسة بسام هاشم";
+  document.getElementById("userName").textContent = "مدرسة منير ايلي داغر";
   document.getElementById("userPhone").textContent = phone;
 
   QUESTIONS = await loadQuestions();
